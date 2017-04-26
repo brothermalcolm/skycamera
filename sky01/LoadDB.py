6ab9ded1ec06e456fb514d42759c0de2eb0160e2
brothermalcolm <malcolmng@alumni.purdue.edu>
2017-04-26 14:20:06 +0800

    Update LoadDB.py
    removed extraneous code

--- sky01/LoadDB.py
+++ sky01/LoadDB.py
@@ -5,35 +5,14 @@ Created on Fri Mar  3 15:14:37 2017
 @author: SERNMO
 """
 #%% Import packages
-import time
-import os
-import pymysql
-from datetime import datetime
-import pandas as pd
 import glob
 import connection
 
-#%%
-# 1) Convert image key (in unix epoch time) to gregorian calander timestamp
-et = time.time()
-print(et)
-ct = time.ctime(et)
-print(ct)
-
-#%%
-# 2) Create table in seriscixi db to store image data, use sp
-
-
-#connect to the database
+#%% connect to the database
 connection = connection.getConnection()
 cursor = connection.cursor()
-#cursor.callproc('sp_create_tbl_skycamera')
-
-connection.commit()
     
-#%%
-# 3) Load image data into table
-#load the text file (first with \r\n line terminators
+#%% Load image data into table
 root = "/data/skycamera/sky01/Data"
 filelist = glob.glob("%s/*.txt" % root)
 filename = max(filelist)
