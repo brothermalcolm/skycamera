faf035d4ae81bdaf4f0aaadc4c7a818acb966678
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-07-25 11:04:59 +0800

    Update ReadSQL.py

--- sky01/ReadSQL.py
+++ sky01/ReadSQL.py
@@ -1,1 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 12:48:23 2017
 
+@author: SERNMO
+
+Sky Camera Project
+
+Read data from a regularly updated MySQL database and
+output to a Python dashboard.
+
+Database query part - defines function for retrieving data associated 
+with the current image. Direct run will return one row of data.
+"""
+
+#%% Import packages
+from connection import getConnection
+
+#%% Read data associated to the current image
+def read_sql(ImageID, VarName):
+    try:
+        conn = getConnection()
+        cur = conn.cursor()
+        args = [ImageID]
+        cur.callproc("sp_join_skyghi", args)
+        result = cur.fetchall()[0]
+        result = result[VarName]
+        conn.close()
+        return result
+    except:
+        print('Have you mispelled the variable name?')
